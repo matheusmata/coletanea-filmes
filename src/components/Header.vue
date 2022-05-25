1bbb9c9d4b168f2b0a1abcfa6caae9c10d289661
matheusmata <matheus.a.mata@gmail.com>
2022-05-24 21:20:23 -0300

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header id="header">  
+      <router-link to="/">CATÁLOGO DE FILMES</router-link>
+      <router-link tag="button" to="/salvos" exact class="meusfilmes">MINHA LISTA</router-link>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  #header{
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    width: 100%;
+    height: 60px;
+    background-color:  brown;
+  }
+  #header a{
+    text-decoration: none;
+    font-size: 30px;
+    color: #fff;
+    cursor: pointer;
+  }
+  button{
+    cursor: pointer;
+    border: 0;
+    background: #fff;
+    padding:  5px 15px;
+    outline: none;
+    border-radius: 5px;
+    font-weight: bold;
+  }
+
+</style>
